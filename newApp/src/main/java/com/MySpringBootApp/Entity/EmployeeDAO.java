297966921227a71a6a71f6cc00579840ffdc873a
Tanmay Chavan <chavantanmays999@gmail.com>
2025-06-16 20:19:11 +0530

    ABC

--- newApp/src/main/java/com/MySpringBootApp/Entity/EmployeeDAO.java
+++ newApp/src/main/java/com/MySpringBootApp/Entity/EmployeeDAO.java
@@ -1,5 +1,16 @@
 package com.MySpringBootApp.Entity;
 
+import java.util.List;
+
 public interface EmployeeDAO {
+    void save(Employee employee);
+
+    Employee findById(int id);
+
+    void deleteById(int id);
+
+    List<Employee> findAll();
+
+    // Additional methods can be defined here as needed
 
 }
